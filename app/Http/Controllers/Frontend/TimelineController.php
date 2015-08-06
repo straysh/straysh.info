bba451df7853d5c9c14d7a5a02ef3ce06a19aeeb
straysh <jobhancao@gmail.com>
2015-08-06 11:30:54 +0800

    原文链接

--- app/Http/Controllers/Frontend/TimelineController.php
+++ app/Http/Controllers/Frontend/TimelineController.php
@@ -14,7 +14,20 @@ class TimelineController extends FrontController
 			return redirect('/');
 		}
 
+        $pattern = '#原文:(.*)\n#i';
+        foreach($article as $item)
+        {
+            preg_match($pattern, $item->content, $matches);
+            unset($matches[0]);
+            if(isset($matches[1]))
+            {
+                $item->link = $matches[1];
+                $item->content = preg_replace($pattern, '', $item->content);
+            }else
+            {
 
+            }
+        }
 		return view('frontend.timeline.index', [
 			'articles' => $article,
 		]);
--- resources/views/frontend/timeline/index.blade.php
+++ resources/views/frontend/timeline/index.blade.php
@@ -4,6 +4,9 @@
     @foreach($articles as $k=>$item)
         <timeline>
             <h1 class="justcenter">{{ $item->title }}</h1>
+            @if(isset($item->link))
+            原文: <a href="{{ $item->link }}" target="_blank">{{ $item->link }}</a>
+            @endif
             <article>{!! ViewHelper::markdownParse($item->content) !!}</article>
         </timeline>
         @if( isset($articles[$k+1]) )
