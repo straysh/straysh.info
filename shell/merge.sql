5e8dac68621ba10150ecdd47b5e8cb95299dc25f
straysh <jobhancao@gmail.com>
2016-06-13 16:35:33 +0800

    数据源更新脚本

---
+++ shell/merge.sql
@@ -0,0 +1,9 @@
+-- 备份原始数据
+mysqldump strayshInfo > storage/strayshInfo.sql;
+mysql straysh_old < storage/strayshInfo.sql;
+
+-- 新数据覆盖
+mysql strayshInfo < storage/t.sql;
+
+-- 恢复hits字段
+update strayshInfo t,straysh_old o set t.hits=o.hits where t.id=o.id;
